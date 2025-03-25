e60a8634e226d7f4598292ac61ad7121774af3b2
Tomáš Křivan <cyan.roke@gmail.com>
2025-03-25 16:45:35 +0100

    Sorting is working

--- bootutil.sh
+++ bootutil.sh
@@ -4,36 +4,52 @@
 #boot_entries_dir="/boot/loader/entries"
 boot_entries_dir="./entries"
 sort_type="none"
+kernel_regex=""
 
-List() {
+function ListFileEntry() {
+    title=$(grep -oP '^title \K.*' $1)
+    version=$(grep -oP '^version \K.*' $1)
+    linux=$(grep -oP '^linux \K.*' $1)
+    echo "$title ($version, $linux)"
+}
+
+function List() {
     if [ "$sort_type" == "none" ]; then
         for file in $boot_entries_dir/*; do
-            title=$(awk -F '^title ' '{printf "%s", $2}' $file)
-            version=$(awk -F '^version ' '{printf "%s", $2}' $file)
-            linux=$(awk -F '^linux ' '{printf "%s", $2}' $file)
-            echo "$title ($version, $linux)"
+            ListFileEntry $file
         done
     fi
 
     if [ "$sort_type" == "file" ]; then
-        for file in $(sort($boot_entries_dir/*)); do
-            title=$(awk -F '^title ' '{printf "%s", $2}' $file)
-            version=$(awk -F '^version ' '{printf "%s", $2}' $file)
-            linux=$(awk -F '^linux ' '{printf "%s", $2}' $file)
-            echo "$title ($version, $linux)"
+        for file in $boot_entries_dir/*; do
+            echo $file | grep -oP '[^/]*\.conf$'
+        done | sort | while read file; do
+            ListFileEntry "$boot_entries_dir/$file"
         done
     fi
 
     if [ "$sort_type" == "sortkey" ]; then
-        echo "todo"
+        for file in $boot_entries_dir/*; do
+            sort_key=$(grep -oP '^sort-key \K.*' $file)
+            echo "$([[ "$sort_key" != "" ]] && echo "1" || echo "a") $sort_key $(echo $file | grep -oP '[^/]*\.conf$')"
+        done | sort | cut -d ' ' -f3- | while read file; do
+            ListFileEntry "$boot_entries_dir/$file"
+        done
+    fi
+
+    if [ "$sort_type" == "kernel" ]; then
+        for file in $boot_entries_dir/*; do
+            grep -oP "^linux \K.*" $file | grep -qP "$kernel_regex" && echo $file
+        done | while read file; do
+            ListFileEntry "$file"
+        done
     fi
 }
 
 mode=$1
 shift
 
-while getopts "fsb:" option; do
-    echo $option
+while getopts "fsb:k:" option; do
     case $option in 
         b)
         boot_entries_dir="$OPTARG"
@@ -43,6 +59,10 @@ while getopts "fsb:" option; do
         ;;
         s)
         sort_type="sortkey"
+        ;;
+        k)
+        sort_type="kernel"
+        kernel_regex="$OPTARG"
         ;;
         \?)
         echo "Error invalid option"
