1fa9542065e156d57ad49dd19656e20cd42788d3
Tomáš Křivan <cyan.roke@gmail.com>
2025-04-02 22:15:37 +0200

    List filtering reworked

--- bootutil.sh
+++ bootutil.sh
@@ -3,9 +3,10 @@
 # Demo only
 #boot_entries_dir="/boot/loader/entries"
 boot_entries_dir="./entries"
-sort_type="none"
-filter_type="none"
-filter_regex=""
+sort_type=""
+filter_type=""
+filter_kernel_regex=""
+filter_title_regex=""
 
 function ListFileEntry() {
     title=$(grep -oP '^title \K.*' $1)
@@ -14,44 +15,76 @@ function ListFileEntry() {
     echo "$title ($version, $linux)"
 }
 
+function FilterKernel() {
+    grep -oP "^linux \K.*" $1 | grep -qE "$filter_kernel_regex"
+}
+
+function FilterTitle() {
+    grep -oP "^title \K.*" $1 | grep -qE "$filter_title_regex"
+}
+
 function List() {
-    if [ "$sort_type" == "none" ]; then
+
+    echo $filter_type | grep -qG "kernel"
+    bykernel=$?
+    echo $filter_type | grep -qG "title" 
+    bytitle=$?
+
+    ( # Filter
+    if [[ $bykernel == 0 ]] && [[ $bytitle == 0 ]]; then
         for file in $boot_entries_dir/*; do
-            ListFileEntry $file
+            FilterKernel $file && FilterTitle $file && echo $file 
+        done
+    elif [[ $bykernel == 0 ]]; then
+        for file in $boot_entries_dir/*; do
+            FilterKernel $file && echo $file 
+        done
+    elif [[ $bytitle == 0 ]]; then
+        for file in $boot_entries_dir/*; do
+            FilterTitle $file && echo $file 
+        done
+    else
+        for file in $boot_entries_dir/*; do
+            echo $file 
         done
     fi
+    ) | while read file; do
+        ListFileEntry $file
+    done
 
-    if [ "$sort_type" == "file" ]; then
+    return
+
+    if [[ "$sort_type" == "file" ]]; then
         for file in $boot_entries_dir/*; do
             echo $file | grep -oP '[^/]*\.conf$'
         done | sort | while read file; do
-            ListFileEntry "$boot_entries_dir/$file"
+             ListFileEntry "$boot_entries_dir/$file"
         done
     fi
 
-    if [ "$sort_type" == "sortkey" ]; then
+    if [[ $sort_type == "sortkey" ]]; then
         for file in $boot_entries_dir/*; do
             sort_key=$(grep -oP '^sort-key \K.*' $file)
             echo "$([[ "$sort_key" != "" ]] && echo "1" || echo "a") $sort_key $(echo $file | grep -oP '[^/]*\.conf$')"
         done | sort | cut -d ' ' -f3- | while read file; do
-            ListFileEntry "$boot_entries_dir/$file"
-        done
+        ListFileEntry "$boot_entries_dir/$file"
+    done
     fi
 
-    if [ "$sort_type" == "kernel" ]; then
+    if [[ $filter_type == "kernel" ]]; then
         for file in $boot_entries_dir/*; do
-            grep -oP "^linux \K.*" $file | grep -qE "$filter_regex" && echo $file
+            FilterKernel $file && echo $file
         done | while read file; do
-            ListFileEntry "$file"
-        done
+        ListFileEntry "$file"
+    done
     fi
 
-    if [ "$sort_type" == "title" ]; then
+    if [[ $filter_type == "title" ]]; then
         for file in $boot_entries_dir/*; do
-            grep -oP "^title \K.*" $file | grep -qE "$filter_regex" && echo $file
+            FilterTitle $file && echo $file
         done | while read file; do
-            ListFileEntry "$file"
-        done
+        ListFileEntry "$file"
+    done
     fi
 }
 
@@ -64,8 +97,10 @@ function Remove() {
     done
 }
 
+mode=$1
 
-# Options
+shift 
+
 while getopts "fsb:k:t:" option; do
     case $option in 
         b)
@@ -78,12 +113,12 @@ while getopts "fsb:k:t:" option; do
             sort_type="sortkey"
             ;;
         k)
-            filter_type="kernel"
-            filter_regex="$OPTARG"
+            filter_type+="kernel"
+            filter_kernel_regex="$OPTARG"
             ;;
         t)
-            filter_type="title"
-            filter_regex="$OPTARG"
+            filter_type+="title"
+            filter_title_regex="$OPTARG"
             ;;
         \?)
             echo "Error invalid option"
@@ -92,31 +127,28 @@ while getopts "fsb:k:t:" option; do
     esac
 done
 
-shift $((OPTIND-1))
-
 # Mode
-case $1 in 
+case $mode in 
     list)
-    List
-    ;;
+        List
+        ;;
     remove)
-    if [[ "$#" == "1" && "$1" != "" ]]; then
-        Remove $1
-    fi
-    ;;
+        if [[ "$#" == "1" && "$1" != "" ]]; then
+            Remove $1
+        fi
+        ;;
     duplicate)
-    echo "mode: duplicate"
-    ;;
+        echo "mode: duplicate"
+        ;;
     show-default)
-    echo "mode: duplicate"
-    ;;
+        echo "mode: duplicate"
+        ;;
     make-default)
-    echo "mode: duplicate"
-    ;;
+        echo "mode: duplicate"
+        ;;
     *)
-    echo "Error invalid mode '$1'"
-    exit
-    ;;
+        echo "Error invalid mode '$1'"
+        exit
+        ;;
 esac
-
 #echo "entries path: '$boot_entries_dir'"
